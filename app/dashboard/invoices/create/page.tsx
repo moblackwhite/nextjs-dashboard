061f5f77b738de2eb03830c00d18215eeea01ffb
HardyHu <1243971719@qq.com>
2026-04-26 20:58:13 +0800

    update suspense

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,23 +1,24 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
- 
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+
 export default async function Page() {
   const customers = await fetchCustomers();
- 
+
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: "Invoices", href: "/dashboard/invoices" },
           {
-            label: 'Create Invoice',
-            href: '/dashboard/invoices/create',
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
             active: true,
           },
         ]}
       />
-      <Form customers={customers} />
+        <Form customers={customers} />
     </main>
   );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,15 +1,15 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import { fetchInvoicesPages } from '@/app/lib/data';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { Metadata } from 'next';
+import Pagination from "@/app/ui/invoices/pagination";
+import { fetchInvoicesPages } from "@/app/lib/data";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Invoices | Acme Dashboard',
+  title: "Invoices | Acme Dashboard",
 };
 
 export default async function Page(props: {
@@ -19,7 +19,7 @@ export default async function Page(props: {
   }>;
 }) {
   const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || "";
   const currentPage = searchParams?.page ? parseInt(searchParams.page) : 1;
   const totalPages = await fetchInvoicesPages(query);
 
